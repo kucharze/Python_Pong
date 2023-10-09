765767ec206487b8766aa2073ff3b6a876701eb9
Zachary Kuchar <zacharyk323@live.com>
2023-10-09 15:40:53 -0400

    Logic for cloning gadget

--- Pong.py
+++ Pong.py
@@ -210,6 +210,28 @@ while run:
             right_gadget_remaining -= 1
     elif gadget_pair == 2:
         print("Ball cloning")
+        if left_gadget == 1:
+            # left paddle
+            if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+                if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+                    ball_x = left_paddle_x + paddle_width
+                    dummy_ball_x = left_paddle_x + paddle_width
+                    ball_vel_x *= -1
+                    dummy_ball_vel_x *= -1
+                    dummy_ball_vel_y *= -2
+                    left_gadget == 0
+                    left_gadget_remaining -= 1
+
+        if right_gadget == 1:
+            # right paddle
+            if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+                if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+                    ball_x = right_paddle_x
+                    dummy_ball_x = right_paddle_x
+                    ball_vel_x *= -1
+                    dummy_ball_vel_x *= -2
+                    right_gadget == 0
+                    right_gadget_remaining -= 1
 
     # Update the display window
     pygame.display.update()
